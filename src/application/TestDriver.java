389abba855bc5cce8f00fdeb8d0974a2800c2363
J05H14 <j05h14@yahoo.com>
2017-06-18 15:35:43 -0700

    started testing

--- src/application/TestDriver.java
+++ src/application/TestDriver.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.Scanner;
+
+import game.RockPaperScissors;
 import opponents.*;
 
 public class TestDriver {
@@ -7,14 +10,18 @@ public class TestDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Opponent p1 = new Rock("Cutman");
-
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
-		System.out.println(p1.move());
+		Scanner in = new Scanner(System.in);
+		
+		RockPaperScissors game = new RockPaperScissors(p1);
+		int move = in.nextInt();
+		
+		try {
+			System.out.println(game.match(move));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
